06e1498ef92b7c0a3c6327a0c3f0778f33cf0c4f
Suraj Bhandarkar <suraj.bhandarkar13@gmail.com>
2022-10-01 13:43:57 +0530

    day based weather update

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
+        const { lat,lon} = data.coord;
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src =
             "https://openweathermap.org/img/wn/" + icon + ".png";
@@ -33,12 +34,43 @@ let weather = {
         document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage =
             "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=metric&appid="+this.apiKey;
+        getWeatherWeekly(url);
+
     },
     search: function() {
         this.fetchWeather(document.querySelector(".search-bar").value);
     },
 };
 
+async function getWeatherWeekly(url)
+{
+    fetch(url).then(res => res.json()).then(data => {
+
+        console.log(data)
+        showWeatherData(data);
+    })
+}
+
+function showWeatherData (data){
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    let otherDayForcast = ''
+    data.daily.forEach((day, idx) => {
+            otherDayForcast += `
+            <div class="weather-forecast-item">
+                <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}.png" alt="weather icon" class="w-icon">
+                <div class="temp">Night - ${day.temp.night}&#176;C</div>
+                <div class="temp">Day - ${day.temp.day}&#176;C</div>
+            </div>
+            
+            `
+    })
+    document.getElementById('weather-forecast').innerHTML = otherDayForcast;
+}
 document.querySelector(".search button").addEventListener("click", function() {
     weather.search();
 });
