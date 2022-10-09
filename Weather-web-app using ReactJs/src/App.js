6f22f8579d14cf227ad10055e80701df0a3805f9
Ayush Yadav <65528804+ayushyadav2001@users.noreply.github.com>
2022-10-09 15:59:27 +0530

    Add files via upload

---
+++ Weather-web-app using ReactJs/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Todo from "./components/todoreact/todo";
+import Temp from "./components/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
