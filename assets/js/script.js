abf1d6e5aaf13bdecc26a69502da6e27f1022eac
Prakash <prakashmtech1998@gmail.com>
2022-10-12 16:41:01 +0530

    cleaned script.js after adding focus on search-bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -210,8 +210,7 @@ function generateWeatherItem(
   day.style.textTransform = "uppercase";
   day.style.fontSize = "20px";
 
-  let newDiv = document.createElement("div");
-  newDiv.className = "image-wrapper"; 
+ 
 
   let icon = document.createElement("img");
   icon.src = `https://openweathermap.org/img/wn/${iconName}.png`;
@@ -245,9 +244,8 @@ function generateWeatherItem(
   nightTemp.style.textTransform="uppercase"
 
   container.appendChild(day);
-  container.appendChild(newDiv);
-  newDiv.appendChild(icon);
- // container.appendChild(icon);
+ 
+  container.appendChild(icon);
   container.appendChild(dayTemp);
   container.appendChild(nightTemp);
   return container;
