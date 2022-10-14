0d5b613b7cc7d23b4e1440b5e1cd2808fef1d548
Prakash <prakashmtech1998@gmail.com>
2022-10-14 16:41:01 +0530

    adding scrollTopFeature

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,7 @@ for (var i in CITY) {
   option.text = CITY[i];
   place.appendChild(option);
 }
+
 function formatAMPM(date) {
   return date.toLocaleString(translations[userLang].formattingLocale, {
     hour: "numeric",
@@ -120,6 +121,7 @@ let weather = {
         this.displayWeather(data);
       });
   },
+
   displayWeather: function (data) {
     //console.log(data);
     const { name } = data;
@@ -323,3 +325,31 @@ function showCurrDay(dayString, dateString, element) {
       "rgba(0, 0, 0, 0.8)";
   }
 }
+
+ 
+ // scrollTop functionality
+ const scrollTop = function () {
+  // create HTML button element
+  const scrollBtn = document.createElement("button");
+  scrollBtn.innerHTML = "&#8679";
+  scrollBtn.setAttribute("id", "scroll-btn");
+  document.body.appendChild(scrollBtn);
+  // hide/show button based on scroll distance
+  const scrollBtnDisplay = function () {
+  window.scrollY > window.innerHeight
+  ? scrollBtn.classList.add("show")
+  : scrollBtn.classList.remove("show");
+  };
+  window.addEventListener("scroll", scrollBtnDisplay);
+  // scroll to top when button clicked
+  const scrollWindow = function () {
+  if (window.scrollY != 0) {
+  setTimeout(function () {
+  window.scrollTo(0, window.scrollY - 50);
+  scrollWindow();
+  }, 10);
+  }
+  };
+  scrollBtn.addEventListener("click", scrollWindow);
+};
+scrollTop();
