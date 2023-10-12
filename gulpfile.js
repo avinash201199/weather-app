f08a74a2c5202dfa5ec5891604d2f67f528cec8b
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2023-10-12 13:36:50 +0530

    [feat]: Gulp integration to weather app

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sync = require("browser-sync").create();
+
+function generateCSS(cb) {
+  src("./sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./assets/css"))
+    .pipe(sync.stream());
+  cb();
+}
+
+exports.css = generateCSS;
+
+function watchFiles(cb) {
+  watch("./sass/*.scss", generateCSS);
+}
+exports.watch = watchFiles;
+
+function browserSync(cb) {
+  sync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  watch("./sass/*.scss", generateCSS);
+  watch("./*.html").on("change", sync.reload);
+}
+
+exports.sync = browserSync;
